66a7cd563db879c6babaa8663fbffb0376de0049
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-11-28 02:28:04 +0300

    first

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function LoginPage() {
-  return <div>you should log in first</div>;
+  return (
+    <div>
+      <div>you should log in first</div>
+      <div>or you don't have the permission</div>
+    </div>
+  );
 }
 
 export default LoginPage;
