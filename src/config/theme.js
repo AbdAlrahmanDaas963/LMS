df070bd917a2a9fb54a6d816809970532881b659
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-11-28 03:26:36 +0300

    theme added

---
+++ src/config/theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    dimblack: {
+      main: "#0C0910",
+    },
+    black: {
+      main: "#0C0C12",
+    },
+    blue: {
+      main: "#3939AA",
+    },
+    yellow: {
+      main: "#FFCF00",
+    },
+    white: {
+      main: "#E9DCC1",
+    },
+  },
+});
+
+export default theme;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./App.jsx";
+
+import "./index.css";
+import theme from "./config/theme.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
